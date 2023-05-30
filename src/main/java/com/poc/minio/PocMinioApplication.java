38fcdcf80fda6c8169868368f0f1e27f7fc68617
Diego397 <diegorey@alu.ufc.br>
2023-05-30 00:00:51 -0300

    implementação dos métodos upload e get_list

---
+++ src/main/java/com/poc/minio/PocMinioApplication.java
@@ -0,0 +1,13 @@
+package com.poc.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMinioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMinioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/minio/PocMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
